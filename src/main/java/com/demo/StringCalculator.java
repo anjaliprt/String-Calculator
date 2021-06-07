a0f5b800f412259fd2a47b71878c377fe0c50704
Anjali <anjalir303@gmail.com>
2021-06-07 08:23:59 +0400

    Number should return same Number Test-commit

--- src/main/java/com/demo/StringCalculator.java
+++ src/main/java/com/demo/StringCalculator.java
@@ -4,7 +4,11 @@ public class StringCalculator {
 
 	
 	public static int sum(String input) {
-		
+		if(input.isEmpty()) {
 			return 0;
 		}
+		else {
+			return Integer.parseInt(input);
+		}
+	}
 }
--- src/test/java/com/demo/StringCalculatorTest.java
+++ src/test/java/com/demo/StringCalculatorTest.java
@@ -7,9 +7,14 @@ import org.junit.jupiter.api.Test;
 class StringCalculatorTest {
 
 	@Test
-	void sumEmptyStringTo0() {
+	void sumsEmptyStringTo0() {
 		assertEquals(0,StringCalculator.sum(""));
 	}
+	@Test
+	void sumsSingleNumberToItself() {
+		assertEquals(3,StringCalculator.sum("3"));
+		assertEquals(56,StringCalculator.sum("56"));
+    }
 }
 
 
